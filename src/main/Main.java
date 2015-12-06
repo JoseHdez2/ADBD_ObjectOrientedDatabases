c2cad508907d7891743a6a0f421a1b6b60225185
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-12-06 22:15:58 +0000

    Consulta de dnis y nombres.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,25 +2,76 @@ package main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-//  Establish a connection to a mSQL database using JDBC
+    
+    static String url = "jdbc:mysql://localhost:3306/javabase";
+    static String username = "java";
+    static String password = "password";
+    
+    static Connection connection;
+    static ResultSet resultados;
+    static int filasResultados;
+    
+    public static void main(String[] args) {
 
-  public static void main (String[] args) { 
-    try
-    {
-      // Step 1: "Load" the JDBC driver
-      Class.forName("com.mysql.jdbc.Driver").newInstance();
+        intentarConexion();
+        
+        //String cod=codigo_e.getText(); // <--- pinta cod, para ver que valor te llega
 
-      // Step 2: Establish the connection to the database 
-      String url = "jdbc:msql:10.0.2.15:3306"; 
-      Connection conn = DriverManager.getConnection(url,"pepe_mar","secreto");  
+        //int numero=Integer.parseInt(cod); // <-- cod llega vacio ""
+
+        consultaDePrueba();
+        
+        mostrarResultados();
     }
-    catch (Exception e)
-    {
-      System.err.println("D'oh! Got an exception!"); 
-      System.err.println(e.getMessage()); 
-    } 
-  } 
+    
+    static public void intentarConexion(){
+        System.out.println("Connecting database...");
+        
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+    }
+    
+    static public void consultaDePrueba(){
+        Statement stmt;
+        ResultSet rs = null, rsn = null;
+        try {
+            stmt = connection.createStatement();
+
+            rs = stmt.executeQuery("SELECT * FROM mi_tabla");
+            resultados = rs;
+            
+            rsn = stmt.executeQuery("SELECT COUNT(*) FROM mi_tabla");
+            rsn.first(); // Movemos el cursor a la primera fila.
+            filasResultados = rsn.getInt(1);
+            
+            // System.out.println(filasResultados);
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+    }
+    
+    static public void mostrarResultados(){
+        try {
+            resultados.first(); // Movemos el cursor a la primera fila.
+            
+            
+        for (int i = 0; i < filasResultados; i++){
+            System.out.println(resultados.getString(i));
+        }
+        
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
